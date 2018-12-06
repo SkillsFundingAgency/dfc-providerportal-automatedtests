138e5db3feadfcf452e79e34af43dba4cb165c6e
Usman Khan <usmanzaheerkhan@gmail.com>
2018-12-06 16:11:48 +0000

    0612-DFC4778 Commit

---
+++ src/UITesting.ProviderPortal/Features/Venue Management/DFC-4778-View All Live Venues.feature
@@ -0,0 +1,13 @@
+﻿Feature: View Live Venues
+	In order to look at all venues
+	As a provider
+	I want to be able to list all my venues
+
+@CI
+Scenario: View Live Venues
+	Given I have accessed the Course Directory
+	And I have searched for a PRN Number "UKPRN Number" 
+	Then all venues for that provider should be displayed with complete address
+	And links to Edit and Archive should be visible
+	And Add Venue Button should be visible
+	And I should be able to view my archived venues
---
+++ src/UITesting.ProviderPortal/Models/Venue Management/Venue_Management.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UITesting.ProviderPortal.Models.Venue_Management
+{
+    public class Venue_Data
+        
+    {
+        public string UKPRNNo = "10003816";
+        public string VenueName = "Aberkenfig Library";
+        public string AddressLine1 = "Aberkenfig Library";
+        public string City = "Bridgend";
+        public string PostCode = "CF32 9PT";
+    }
+}
