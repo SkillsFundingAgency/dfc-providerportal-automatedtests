26f6cb3cd5e648ea134e4d16d75f61b8a2a748eb
kadir_malik <kadir_malik@hotmail.com>
2018-12-06 16:00:53 +0000

    addition of Add Venue Pages

---
+++ src/UITesting.ProviderPortal/Pages/Venue Management/AddVenuePage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UITesting.Framework.Helpers;
+using UITesting.ProviderPortal.TestSupport;
+using OpenQA.Selenium;
+
+namespace UITesting.ProviderPortal.VenueManagement.Pages
+{
+    public class AddVenuePage : BasePage
+    {
+        private static String PAGE_TITLE = "Add venue";
+
+        public AddVenuePage(IWebDriver webDriver) : base(webDriver)
+        {
+            SelfVerify();
+        }
+
+        protected override bool SelfVerify()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+
+        private By venueName = By.Id("name");
+        private By venuePostcode = By.Id("address-postcode");
+        private By FindAddressButton = By.Id("find-address"); 
+
+		public AddVenuePage EnterVenueName(String name)
+        {
+            FormCompletionHelper.EnterText(venueName, name);
+            return new AddVenuePage(webDriver);
+        }
+
+        public AddVenuePage EnterPostCode(String postcode)
+        {
+            FormCompletionHelper.EnterText(venuePostcode, postcode);
+            return new AddVenuePage(webDriver);
+        }
+
+        public AddVenuePage ClickFindAddress()
+        {
+            FormCompletionHelper.ClickElement(FindAddressButton);
+            return new AddVenuePage(webDriver);
+        }
+
+
+    }
+}
---
+++ src/UITesting.ProviderPortal/Pages/Venue Management/AddVenueSelectAddressPage - Copy.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UITesting.Framework.Helpers;
+using UITesting.ProviderPortal.TestSupport;
+using OpenQA.Selenium;
+
+namespace UITesting.ProviderPortal.VenueManagement.Pages
+{
+    public class AddVenueSelectAddressPage : BasePage
+    {
+        private static String PAGE_TITLE = "Add a venue";
+
+        public AddVenueSelectAddressPage(IWebDriver webDriver) : base(webDriver)
+        {
+            SelfVerify();
+        }
+
+        protected override bool SelfVerify()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+
+        private By venueName = By.Id("name");
+        private By addressSelector = By.Id("address-selector");
+        private By continueButton = By.Id("continue"); 
+
+		public AddVenueSelectAddressPage EnterVenueName(String name)
+        {
+            FormCompletionHelper.EnterText(venueName, name);
+            return new AddVenueSelectAddressPage(webDriver);
+        }
+
+        public AddVenueSelectAddressPage selectFirstAddress()
+        {
+            // TODO SELECT FIRST ADDRESS
+            return new AddVenueSelectAddressPage(webDriver);
+        }
+
+        public AddVenueSelectAddressPage ClickContinue()
+        {
+            FormCompletionHelper.ClickElement(continueButton);
+            return new AddVenueSelectAddressPage(webDriver);
+        }
+
+
+    }
+}
