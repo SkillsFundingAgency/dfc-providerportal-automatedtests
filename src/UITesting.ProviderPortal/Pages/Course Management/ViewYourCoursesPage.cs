d38fff76e79585f51fbc85574ddc6f59ec8fafce
Usman Khan <usmanzaheerkhan@gmail.com>
2019-01-07 12:12:34 +0000

    0701

---
+++ src/UITesting.ProviderPortal/Pages/Course Management/ViewYourCoursesPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UITesting.Framework.Helpers;
+
+namespace UITesting.ProviderPortal.Pages.Course_Management
+{
+    
+    public class ViewYourCoursesPage : TestSupport.BasePage 
+    {
+        private static String PAGE_TITLE = "Your courses";
+        private static By ViewCourseDescriptionLink = By.XPath("//*[@id='main-content']/div/div/div[2]/div/ul/li[2]/div/div/p/a");
+        public ViewYourCoursesPage(IWebDriver webDriver):base(webDriver)
+        {
+            SelfVerify();
+        }
+        protected override bool SelfVerify()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+        internal void CheckViewCourseDescription()
+        {
+            PageInteractionHelper.IsElementPresent(ViewCourseDescriptionLink);
+        }
+    }
+}
