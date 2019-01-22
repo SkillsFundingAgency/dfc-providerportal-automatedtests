d31f035e193e77747f9a7d19c04102eb8d0b1342
kadir_malik <kadir_malik@hotmail.com>
2019-01-22 10:58:54 +0000

    KM - Browserstack include Add Course journey

--- src/UITesting.BrowserStack/StepDefinitions/BrowserStackSteps.cs
+++ src/UITesting.BrowserStack/StepDefinitions/BrowserStackSteps.cs
@@ -3,6 +3,7 @@ using UITesting.Framework.Helpers;
 using UITesting.ProviderPortal.Pages.Venue_Management;
 using UITesting.ProviderPortal.Pages.Provider_Management;
 using UITesting.ProviderPortal.Pages.Qualification_Management;
+using UITesting.ProviderPortal.Pages.Course_Management;
 using UITesting.BrowserStack.TestSupport;
 using OpenQA.Selenium;
 using TechTalk.SpecFlow;
@@ -389,6 +390,300 @@ namespace UITesting.BrowserStack.StepDefinitions
 
         #endregion Qualification
 
+        # region Courses
+
+        [Given(@"I have specified who the course is for (.*)")]
+        public void GivenIHaveSpecifiedWhoTheCourseIsFor(string p0)
+        {
+            if (p0 == "blank")
+            {
+                p0 = "";
+            }
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.EnterWhoCourseFor(p0);
+        }
+
+        [Given(@"I have entered the entry requirements (.*)")]
+        public void GivenIHaveEnteredTheEntryRequirements(string p0)
+        {
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.EnterEntryRequirements(p0);
+
+        }
+
+        [Given(@"I have entered what the student will learn (.*)")]
+        public void GivenIHaveEnteredWhatTheStudentWillLearn(string p0)
+        {
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.EnterWhatWillLearn(p0);
+        }
+
+        [Given(@"I have entered how the student will learn (.*)")]
+        public void GivenIHaveEnteredHowTheStudentWillLearn(string p0)
+        {
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.EnterHowYoullLearn(p0);
+        }
+
+        [Given(@"I have entered what equipment will be needed (.*)")]
+        public void GivenIHaveEnteredWhatEquipmentWillBeNeeded(string p0)
+        {
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.EnterEquipmentNeeded(p0);
+        }
+
+        [Given(@"I have entered how the student will be assessed (.*)")]
+        public void GivenIHaveEnteredHowTheStudentWillBeAssessed(string p0)
+        {
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.HowAssessed(p0);
+        }
+
+
+        [Given(@"I have enter the next steps following completion of the course (.*)")]
+        public void NextStepsFollowingCompletionOfTheCourseLevelOfThisCourse(string p0)
+        {
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.NextSteps(p0);
+        }
+
+
+        [When(@"I click Next Button")]
+        public void WhenIClickNextButton()
+        {
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.ClickNext();
+        }
+
+        [When(@"I click Next Button Failure")]
+        public void WhenIClickNextButtonFail()
+        {
+            AddCoursePage AddCoursePage = new AddCoursePage(webDriver);
+            AddCoursePage.ClickNextFail();
+        }
+
+        [Then(@"Page 1 error message for field (.*) states (.*)")]
+        public void ConfirmErrorMessage(string field, string errorMsg)
+        {
+            AddCoursePage addCoursePage = new AddCoursePage(webDriver);
+            addCoursePage.ConfirmErrorMessage(field, errorMsg);
+        }
+
+
+        [Then(@"Page 1 info message for field (.*) states (.*)")]
+        public void ConfirmInfoMessage(string field, string errorMsg)
+        {
+            AddCoursePage addCoursePage = new AddCoursePage(webDriver);
+            addCoursePage.ConfirmInfoMessage(field, errorMsg);
+        }
+
+        [Given(@"I have navigated to the Add Course Section 2 page")]
+        public void NavigateToAddCoursePage()
+        {
+            webDriver.Url = Configurator.GetConfiguratorInstance().GetBaseUrl() + "//Courses//AddCourseSection2";
+        }
+
+        [When(@"I enter the following in the course name field Course (.*)")]
+        public void EntereCourseNameField(string coursename)
+        {
+            if (coursename == "blank")
+            {
+                coursename = "";
+            }
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.EnterCourseName(coursename);
+        }
+
+
+        [Then(@"course name error validation is not displayed")]
+        public void ThenCourseNameIsNotDisplayed()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.CourseNameErrorNotDisplayed();
+        }
+
+        [Then(@"course name error validation is displayed")]
+        public void ThenCourseNameErrorDisplayed()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.CourseNameErrorDisplayed();
+        }
+
+        [When(@"I enter the following in the URL field (.*)")]
+        public void WhenIEnterTheURL(string url)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.EnterCourseURL(url);
+        }
+
+        [Then(@"course URL error validation is displayed")]
+        public void ThenCourseURLErrorValidationIsDisplayed()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.CourseURLErrorDisplayed();
+        }
+
+
+        [Then(@"error message for field (.*) states (.*)")]
+        public void ConfirmErrorMessagePage2(string field, string errorMsg)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.ConfirmErrorMessage(field, errorMsg);
+        }
+
+
+        [When(@"I enter the following in the cost field (.*)")]
+        public void EnterTheFollowingInTheCostField(string cost)
+        {
+            if (cost == "blank")
+            {
+                cost = "";
+            }
+
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.EnterCourseCost(cost);
+        }
+
+        [Then(@"course Cost error validation is displayed")]
+        public void CourseCostErrorValidationIsDisplayed()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.CourseCostErrorDisplayed();
+        }
+
+
+        [When(@"I enter the following in the course id field (.*)")]
+        public void EnterCourseID(string courseId)
+        {
+            if (courseId == "blank")
+            {
+                courseId = "";
+            }
+
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.EnterCourseId(courseId);
+        }
+
+
+        [Then(@"course id error validation is displayed")]
+        public void ThenCourseIDErrorDisplayed()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.CourseIDErrorDisplayed();
+        }
+
+        [When(@"I enter the following in the cost description field (.*)")]
+        public void EnterCostDescription(string costdescription)
+        {
+            if (costdescription == "blank")
+            {
+                costdescription = "";
+            }
+
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.EnterCostDescription(costdescription);
+        }
+
+        [Then(@"cost description error validation is displayed")]
+        public void CostDescriptionErrorValidationIsDisplayed()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.CostDescriptionErrorDisplayed();
+        }
+
+        [Then(@"info message for field (.*) states (.*)")]
+        public void InfoMessageForFieldCostDescription(string field, string infoMsg)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.ConfirmInfoMessage(field, infoMsg);
+        }
+
+        [When(@"I Select attendance mode (.*)")]
+        public void WhenISelectAttendanceMode(string attendance)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.SelectAttendance(attendance);
+        }
+
+        [When(@"I Select study mode (.*)")]
+        public void WhenISelectStudyMode(string studymode)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.SelectStudyMode(studymode);
+        }
+
+        [When(@"I print Qualification details")]
+        public void WhenIPrintQualificationDetails()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.GetQualifiactionDetails();
+        }
+
+        [When(@"I tick the Advanced Learner Loans option")]
+        public void TickTheAdvancedLearnerLoansOption()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.SelectAdvancedLearnerLoan();
+        }
+
+        [When(@"I select start date (.*)")]
+        public void WhenISelectStartDate(string StartDate)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.SelectStartDateType(StartDate);
+        }
+
+        [When(@"I enter (.*) in the (.*) field")]
+        public void WhenIEnterDay(string value, string field)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.EnterStartDate(value, field);
+        }
+
+        [When(@"I select duration length (.*)")]
+        public void WhenISelectDurationLength(string length)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.EnterDuration(length);
+        }
+
+        [When(@"I select duration unit (.*)")]
+        public void WhenISelectDurationUnit(string unit)
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.SelectDurationUnit(unit);
+        }
+
+        [When(@"I click Publish")]
+        public void WhenIClickPublish()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.PublishCourse();
+        }
+
+        [When(@"I click Publish leading to error")]
+        public void WhenIClickPublishLeadingToError()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.PublishCourseError();
+        }
+
+
+        [When(@"I select the first venue in the venues list")]
+        public void SelectTheFirstVenueInTheVenuesList()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.SelectFirstVenue();
+        }
+
+        [When(@"I select the second venue in the venues list")]
+        public void SelectTheSecondVenueInTheVenuesList()
+        {
+            AddCoursePage2 addCoursePage2 = new AddCoursePage2(webDriver);
+            addCoursePage2.SelectSecondVenue();
+        }
+
+        #endregion Courses
 
     }
 }
--- src/UITesting.ProviderPortal/Pages/Course Management/ViewYourCoursesPage.cs
+++ src/UITesting.ProviderPortal/Pages/Course Management/ViewYourCoursesPage.cs
@@ -13,6 +13,7 @@ namespace UITesting.ProviderPortal.Pages.Course_Management
     public class ViewYourCoursesPage : TestSupport.BasePage
     {
         private static String PAGE_TITLE = "Your courses";
+        private By successMsg = By.XPath(".//*[@id='main-content']/div/div/div[1]/h1");
         private static By ViewCourseDescriptionLink = By.XPath("//*[@id='0']");       //first one in list '0'
         private static By showFirstCourseDescriptionPopupLink = By.XPath("//*[@id='0']");
         private static By showSecondCourseDescriptionPopupLink = By.XPath("//*[@id='1']");
@@ -102,7 +103,10 @@ namespace UITesting.ProviderPortal.Pages.Course_Management
 
         public ViewYourCoursesPage(IWebDriver webDriver) : base(webDriver)
         {
-            SelfVerify();
+            if (!PageInteractionHelper.IsElementPresent(successMsg))
+            {
+                SelfVerify();
+            }           
         }
 
         internal void ClickCloseCostDescription()
