23ee600b33acb5b23f0562c92025cfa427228760
kadir_malik <kadir_malik@hotmail.com>
2019-01-28 14:30:17 +0000

    amend venues tests following changes to venues screens

--- src/UITesting.ProviderPortal/Pages/Venue Management/ViewAllLiveVenuesPage.cs
+++ src/UITesting.ProviderPortal/Pages/Venue Management/ViewAllLiveVenuesPage.cs
@@ -12,7 +12,7 @@ namespace UITesting.ProviderPortal.Pages.Venue_Management
     {
         private static String PAGE_TITLE = "Your Venues";
         private static String PAGE_TITLE_ELEMENT = "//*[@id='main-content']/div/div[1]/div/div/h1";
-        private static String VENUE_DATA = "//*[@id='live']/div[1]/label";  //*[@id="live"]/div[1]/label
+        private static String VENUE_DATA = "//*[@id='live']/table/tbody/tr[1]/td[2]";  //"//*[@id='live']/div[1]/label"
         private static String ARCH_TAB= "//*[@id='tab_archived']";
         private static String ADD_VENUE_BUTTON = "//*[@id='venueSearchResultForm']/div/div/div/a[2]/div/button";  //*[@id="venueSearchResultForm"]/div/div/div/a[2]/div/button
         private By venueBanner = By.XPath(".//*[@id='qual']");
