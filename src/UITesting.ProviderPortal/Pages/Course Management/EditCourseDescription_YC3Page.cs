df713a6a34eb5aaf7660a1f2d5fbc31bfe3b7594
Usman Khan <usmanzaheerkhan@gmail.com>
2019-03-04 16:24:27 +0000

    YC3-EditCourseDesc

---
+++ src/UITesting.ProviderPortal/Pages/Course Management/EditCourseDescription_YC3Page.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UITesting.Framework.Helpers;
+using UITesting.ProviderPortal.TestSupport;
+
+namespace UITesting.ProviderPortal.Pages.Course_Management
+{
+    
+
+    public class EditCourseDescription_YC3Page: BasePage
+    {
+        private static String PAGE_TITLE = "Edit Course";
+        public EditCourseDescription_YC3Page(IWebDriver webDriver): base(webDriver)
+        {
+            SelfVerify();
+        }
+        protected override bool SelfVerify()
+        {
+            return PageInteractionHelper.VerifyPageHeading(this.GetPageHeading(), PAGE_TITLE);
+        }
+    }
+}
