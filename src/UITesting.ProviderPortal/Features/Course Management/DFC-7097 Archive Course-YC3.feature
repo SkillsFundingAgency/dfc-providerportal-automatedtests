b1fa2fc50433d89c54b71c4657510430ad022832
Usman Khan <usmanzaheerkhan@gmail.com>
2019-03-13 14:05:35 +0000

    Archive

---
+++ src/UITesting.ProviderPortal/Features/Course Management/DFC-7097 Archive Course-YC3.feature
@@ -0,0 +1,15 @@
+﻿Feature: DFC-7097 Archive Course-YC3
+	In order to archive a course
+	As a provider
+	I want to be able to archive a course using Archive button
+
+Background: 
+Given I have accessed course directory as a provider
+	And I login as admin with user and password
+	Then I am logged in
+	Given I have searched for UKPRN "10002815" and clicked search
+	When I click to view my courses
+	And I select Edit Course Description
+	Then Edit Course screen should be displayed
+@CI
+Scenario: 
---
+++ src/UITesting.ProviderPortal/Features/Course Management/DFC-7097 Archive Course-YC3.feature.cs
@@ -0,0 +1,37 @@
+﻿#error Generation error: Message: The scenario must have a title specified.
+
+AppDomain Information: 
+	Name: AppDomainForTestGeneration
+	ConfigFile: C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\plugincompability.config
+	BaseDirectory: C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools
+
+Loaded Assemblies:
+Fullname | Location | Codebase
+CommandLine, Version=2.2.1.0, Culture=neutral, PublicKeyToken=de6f01bd326f8c32 | C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\CommandLine.dll | file:///C:/Users/Usman/.nuget/packages/specflow/2.4.1/tools/CommandLine.DLL
+CommandLine, Version=2.3.0.0, Culture=neutral, PublicKeyToken=de6f01bd326f8c32 | c:\users\usman\appdata\local\microsoft\visualstudio\15.0_a41fa96b\extensions\qoyqdvim.rzz\CommandLine.dll | file:///c:/users/usman/appdata/local/microsoft/visualstudio/15.0_a41fa96b/extensions/qoyqdvim.rzz/CommandLine.DLL
+Gherkin, Version=5.0.0.0, Culture=neutral, PublicKeyToken=86496cfa5b4a5851 | C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\Gherkin.dll | file:///C:/Users/Usman/.nuget/packages/specflow/2.4.1/tools/Gherkin.DLL
+mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\Windows\Microsoft.NET\Framework\v4.0.30319\mscorlib.dll | file:///C:/Windows/Microsoft.NET/Framework/v4.0.30319/mscorlib.dll
+Newtonsoft.Json, Version=10.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed | C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\Newtonsoft.Json.dll | file:///C:/Users/Usman/.nuget/packages/specflow/2.4.1/tools/Newtonsoft.Json.DLL
+Newtonsoft.Json, Version=9.0.0.0, Culture=neutral, PublicKeyToken=30ad4fe6b2a6aeed | c:\users\usman\appdata\local\microsoft\visualstudio\15.0_a41fa96b\extensions\qoyqdvim.rzz\Newtonsoft.Json.dll | file:///c:/users/usman/appdata/local/microsoft/visualstudio/15.0_a41fa96b/extensions/qoyqdvim.rzz/Newtonsoft.Json.DLL
+System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\WINDOWS\Microsoft.Net\assembly\GAC_MSIL\System\v4.0_4.0.0.0__b77a5c561934e089\System.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System/v4.0_4.0.0.0__b77a5c561934e089/System.dll
+System.Configuration, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a | C:\WINDOWS\Microsoft.Net\assembly\GAC_MSIL\System.Configuration\v4.0_4.0.0.0__b03f5f7f11d50a3a\System.Configuration.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System.Configuration/v4.0_4.0.0.0__b03f5f7f11d50a3a/System.Configuration.dll
+System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\WINDOWS\Microsoft.Net\assembly\GAC_MSIL\System.Core\v4.0_4.0.0.0__b77a5c561934e089\System.Core.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System.Core/v4.0_4.0.0.0__b77a5c561934e089/System.Core.dll
+System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\WINDOWS\Microsoft.Net\assembly\GAC_32\System.Data\v4.0_4.0.0.0__b77a5c561934e089\System.Data.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_32/System.Data/v4.0_4.0.0.0__b77a5c561934e089/System.Data.dll
+System.Numerics, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\WINDOWS\Microsoft.Net\assembly\GAC_MSIL\System.Numerics\v4.0_4.0.0.0__b77a5c561934e089\System.Numerics.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System.Numerics/v4.0_4.0.0.0__b77a5c561934e089/System.Numerics.dll
+System.Runtime.Serialization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\WINDOWS\Microsoft.Net\assembly\GAC_MSIL\System.Runtime.Serialization\v4.0_4.0.0.0__b77a5c561934e089\System.Runtime.Serialization.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System.Runtime.Serialization/v4.0_4.0.0.0__b77a5c561934e089/System.Runtime.Serialization.dll
+System.ValueTuple, Version=4.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51 | C:\WINDOWS\Microsoft.Net\assembly\GAC_MSIL\System.ValueTuple\v4.0_4.0.0.0__cc7b13ffcd2ddd51\System.ValueTuple.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System.ValueTuple/v4.0_4.0.0.0__cc7b13ffcd2ddd51/System.ValueTuple.dll
+System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\WINDOWS\Microsoft.Net\assembly\GAC_MSIL\System.Xml\v4.0_4.0.0.0__b77a5c561934e089\System.Xml.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System.Xml/v4.0_4.0.0.0__b77a5c561934e089/System.Xml.dll
+System.Xml.Linq, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089 | C:\WINDOWS\Microsoft.Net\assembly\GAC_MSIL\System.Xml.Linq\v4.0_4.0.0.0__b77a5c561934e089\System.Xml.Linq.dll | file:///C:/WINDOWS/Microsoft.Net/assembly/GAC_MSIL/System.Xml.Linq/v4.0_4.0.0.0__b77a5c561934e089/System.Xml.Linq.dll
+TechTalk.SpecFlow, Version=2.4.0.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\TechTalk.SpecFlow.dll | file:///C:/Users/Usman/.nuget/packages/specflow/2.4.1/tools/TechTalk.SpecFlow.DLL
+TechTalk.SpecFlow.Generator, Version=2.4.0.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\TechTalk.SpecFlow.Generator.dll | file:///C:/Users/Usman/.nuget/packages/specflow/2.4.1/tools/TechTalk.SpecFlow.Generator.DLL
+TechTalk.SpecFlow.Parser, Version=2.4.0.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\TechTalk.SpecFlow.Parser.dll | file:///C:/Users/Usman/.nuget/packages/specflow/2.4.1/tools/TechTalk.SpecFlow.Parser.DLL
+TechTalk.SpecFlow.Reporting, Version=2.4.0.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\TechTalk.SpecFlow.Reporting.dll | file:///C:/Users/Usman/.nuget/packages/specflow/2.4.1/tools/TechTalk.SpecFlow.Reporting.DLL
+TechTalk.SpecFlow.Utils, Version=2.4.0.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | C:\Users\Usman\.nuget\packages\specflow\2.4.1\tools\TechTalk.SpecFlow.Utils.dll | file:///C:/Users/Usman/.nuget/packages/specflow/2.4.1/tools/TechTalk.SpecFlow.Utils.DLL
+TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator, Version=2017.2.7.0, Culture=neutral, PublicKeyToken=0778194805d6db41 | c:\users\usman\appdata\local\microsoft\visualstudio\15.0_a41fa96b\extensions\qoyqdvim.rzz\TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.exe | file:///c:/users/usman/appdata/local/microsoft/visualstudio/15.0_a41fa96b/extensions/qoyqdvim.rzz/TechTalk.SpecFlow.VisualStudio.CodeBehindGenerator.exe
+
+TechTalk.SpecFlow.Generator.TestGeneratorException: The scenario must have a title specified.
+   at TechTalk.SpecFlow.Generator.UnitTestFeatureGenerator.GenerateUnitTestFixture(SpecFlowDocument document, String testClassName, String targetNamespace)
+   at TechTalk.SpecFlow.Generator.TestGenerator.GenerateTestFileCode(FeatureFileInput featureFileInput)
+   at TechTalk.SpecFlow.Generator.TestGenerator.GetGeneratedTestCode(FeatureFileInput featureFileInput)
+   at TechTalk.SpecFlow.Generator.TestGenerator.GenerateTestFileWithExceptions(FeatureFileInput featureFileInput, GenerationSettings settings)
+   at TechTalk.SpecFlow.Generator.ErrorHandlingTestGenerator.GenerateTestFile(FeatureFileInput featureFileInput, GenerationSettings settings)
