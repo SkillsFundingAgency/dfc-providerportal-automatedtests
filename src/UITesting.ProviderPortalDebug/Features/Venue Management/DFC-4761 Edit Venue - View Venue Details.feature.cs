eb8c576a3f85d3414ff96ff204d9cac55d37dfd6
kadir_malik <kadir_malik@hotmail.com>
2019-04-12 14:07:31 +0100

    fix debug tests

--- src/UITesting.ProviderPortalDebug/Features/Venue Management/DFC-4761 Edit Venue - View Venue Details.feature.cs
+++ src/UITesting.ProviderPortalDebug/Features/Venue Management/DFC-4761 Edit Venue - View Venue Details.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace UITesting.ProviderPortal.Features.VenueManagement
+namespace UITesting.ProviderPortalDebug.Features.VenueManagement
 {
     using TechTalk.SpecFlow;
     
@@ -133,3 +133,4 @@ this.FeatureBackground();
 }
 #pragma warning restore
 #endregion
+
---
+++ src/UITesting.ProviderPortalDebug/TestSupport/BasePage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using SeleniumExtras.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using TechTalk.SpecFlow;
+
+namespace UITesting.ProviderPortalDebug.Debug.TestSupport
+{
+    public abstract class BasePage
+    {
+        protected IWebDriver webDriver;
+	    private readonly By pageHeading = By.CssSelector("h1");
+
+		public BasePage(IWebDriver webDriver)
+        {
+            this.webDriver = webDriver;
+            SeleniumExtras.PageObjects.PageFactory.InitElements(webDriver, this);
+        }
+
+        protected abstract Boolean SelfVerify();
+
+        protected String GetPageHeading()
+        {
+			return webDriver.FindElement(pageHeading).GetAttribute("innerText");
+		}
+	}
+}
